0768b2cf39695fd7e9dc0426b967a2df12342de5
KeebleKapa <keeblekapa@gmail.com>
2024-08-17 18:27:22 -0400

    Main workspace set up

---
+++ src/main/java/net/keeblekapa/eldritchrealms/EldritchRealms.java
@@ -0,0 +1,16 @@
+package net.keeblekapa.eldritchrealms;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EldritchRealms implements ModInitializer {
+	public static final String MOD_ID = "eldritchrealms";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/keeblekapa/eldritchrealms/EldritchRealmsClient.java
@@ -0,0 +1,10 @@
+package net.keeblekapa.eldritchrealms;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EldritchRealmsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
